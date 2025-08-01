6d50fb9674e35d13386e427e415aba670f9871fa
obegarde <5763603+Obegarde@users.noreply.github.com>
2025-08-01 18:23:00 +0200

    add working calls to brave api

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const apikey = process.env.brave_key
 
 app.use(express.static('public'));
 app.listen(3000, () => console.log('Server running'));
 app.get('/search', async (req, res) => {
-  const searchParams = new URLSearchParams(req.query)
-  console.log(searchParams.values())
-
-  res.send('testString')
+  const queryJoined = req.query['query'].split(',').join('+')
+  const url = `https://api.search.brave.com/res/v1/web/search?q=${queryJoined}`
+  console.log(url)
+  const searchHeaders = new Headers();
+  searchHeaders.append("Content-Type", "application/json");
+  searchHeaders.append("X-Subscription-Token", apikey);
+  searchHeaders.append("Accept-Encoding", "gzip");
+  const searchResponse = await fetch(url, { method: "GET", headers: searchHeaders });
+  console.log(await searchResponse.text());
 });
