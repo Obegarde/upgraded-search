82b7eeb525a2b63211f73ec90efcb1a6c2968a19
obegarde <5763603+Obegarde@users.noreply.github.com>
2025-08-07 16:22:14 +0200

    Add better errorhandling when interacting with ai

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ async function testAI() {
     model: 'gemini-2.0-flash',
     contents:
       'Search Query: why+is+the+sky+blue Result Text: Because of magic',
-    config: { systemInstruction: "You are part of a search engine. You have to look at the page and give a recommendation on whether it is an understandable,good, up to date resource for the given query. Give a summary and your verdict in 5 lines." },
+    config: { systemInstruction: "You are part of a search engine. You have to look at the page and give a recommendation on whether it is an understandable,good, up to date resource for the given query. Give a summary and your verdict in 8 lines." },
   });
 
   console.debug(response.text);
@@ -32,17 +32,24 @@ async function processSearchResults(searchResultsJSON, searchQuery) {
   const outputResults = []
   for (let i = 0; i < amountOfResultsToProcess; i++) {
     const resultObject = { url: searchResultsJSON.web.results[i].url, title: searchResultsJSON.web.results[i].title }
-    const webResponse = await fetch(searchResultsJSON.web.results[i].url);
-    if (!webResponse.ok) {
-      continue;
-    } else {
+    try {
+      const webResponse = await fetch(searchResultsJSON.web.results[i].url);
       const webText = await webResponse.text()
-      const cleanText = convert(webText)
-      const aiResponse = getAiResponse(cleanText, searchQuery)
-      resultObject.summary = await aiResponse
-      console.log(aiResponse)
+      const cleanText = convert(webText, { selectors: [{ selector: 'header', format: 'skip' }, { selector: 'footer', format: 'skip' }] })
+      console.log(cleanText)
+      try {
+        const aiResponseText = await getAiResponseText(cleanText, searchQuery)
+        resultObject.summary = aiResponseText
+      } catch (error) {
+        console.log(error)
+        resultObject.summary = convert(searchResultsJSON.web.results[i].description)
+      }
       outputResults.push(resultObject)
+    } catch (error) {
+      console.log(error)
+      continue;
     }
+
   }
   return outputResults
 }
@@ -51,11 +58,12 @@ async function processSearchResults(searchResultsJSON, searchQuery) {
 
 
 
-async function getAiResponse(pageText, searchQuery) {
+async function getAiResponseText(pageText, searchQuery) {
+  throw new Error('test')
   const response = await ai.models.generateContent({
     model: 'gemini-2.0-flash',
     contents: `Search Query:${searchQuery} Result Text:${pageText}`,
-    config: { systemInstruction: "You are part of a search engine. Give a short summary of the page and give a recommendation on whether it is up to date and worthwhile resource for the given query. Give your verdict in 5 lines. Do not add any formatting to your response." },
+    config: { systemInstruction: "You are part of a search engine. Give a short summary of the page and give a recommendation on whether it is a wellwritten resource for the given query. Give your summary and verdict in 5 lines. Do not add any formatting to your response." },
   });
   return response.text
 }
